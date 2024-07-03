a91e6930c855ec225c37ecde73d07632763c0244
unknown <albertkao227@gmail.com>
2024-07-03 22:12:27 +0800

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # VAE-for-regression-pytorch
+
+Pytorch implementation of **Variational AutoEncoder For Regression: Application to Brain Aging Analysis** by Zhao et al. 
+
+The proposed generative process explicitly models the conditional distribution of letent representations with respect to the regression target variable. Performing a variational inference procedure on this model leads to joint regularization between the VAER and a neural network regressor. The proposed approach is more accurate than state of the art models, and the disentanglement of age in latent representatoins allows for intuitive interpretation of structural development patterns of the human brain. 
+
+- Paper https://arxiv.org/abs/1904.05948
+
+- Repository https://github.com/QingyuZhao/VAE-for-Regression
---
+++ model.py
@@ -0,0 +1,64 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+
+
+class VAERegressor(nn.Module):
+    def __init__(self):
+        super(VAERegressor, self).__init__()
+        self.fc1 = nn.Linear(1, 400)
+        self.fc21 = nn.Linear(400, 20)
+        self.fc22 = nn.Linear(400, 20)
+        self.fc3 = nn.Linear(20, 400)
+        self.fc4 = nn.Linear(400, 1) 
+
+    def encode(self, x):
+        h1 = F.relu(self.fc1(x))
+        return self.fc21(h1), self.fc22(h1)
+    
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+        return mu + eps*std
+    
+    def decode(self, z):
+        h3 = F.relu(self.fc3(z))
+        return self.fc4(h3)
+    
+    def forward(self, x):
+        mu, logvar = self.encode(x)
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z), mu, logvar
+    
+    def loss_function(self, recon_x, x, mu, logvar):
+        BCE = F.mse_loss(recon_x, x, reduction='sum')
+        KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+        return BCE + KLD    
+    
+
+def train(model, device, train_loader, optimizer, epoch):
+    model.train()
+    train_loss = 0
+    for batch_idx, (data, _) in enumerate(train_loader):
+        data = data.to(device)
+        optimizer.zero_grad()
+        recon_batch, mu, logvar = model(data)
+        loss = model.loss_function(recon_batch, data, mu, logvar)
+        loss.backward()
+        train_loss += loss.item()
+        optimizer.step()
+        if batch_idx % 10 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader),
+                loss.item() / len(data)))
+
+    print('====> Epoch: {} Average loss: {:.4f}'.format(
+          epoch, train_loss / len(train_loader.dataset)))
+
+
+
+
+
