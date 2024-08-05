b65b7b43a412b4d1ee1326de0e98562e9db099ad
unknown <albertkao227@gmail.com>
2024-08-05 23:29:34 +0800

    Define encoder class

--- vae_for_regression.py
+++ vae_for_regression.py
@@ -24,10 +24,11 @@ def sampling(mean, log_var):
 def augment_by_transformation(data,age,n):
     augment_scale = 1
     if n <= data.shape[0]:
-        return data
+        return data+
     else:
         raw_n = data.shape[0]
-        m = n - raw_n
+        m = n - raw_n.
+        0 
         for i in range(0,m):
             new_data = np.zeros((1,data.shape[1],data.shape[2],data.shape[3],1))
             idx = np.random.randint(0,raw_n)
@@ -45,6 +46,80 @@ def augment_by_transformation(data,age,n):
 class Encoder(nn.Module):
     def __init__(self, input_shape_x, intermediate_dim, latent_dim):
         super(Encoder, self).__init__()
+
+        self.conv1 = nn.Conv2d(input_shape_x, ft_bank_baseline, kernel_size=3, padding='same')
+        self.act1 = nn.ReLU()
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2)
+
+        self.conv2 = nn.Conv2d(ft_bank_baseline, ft_bank_baseline*2, kernel_size=3, padding='same')
+        self.act2 = nn.ReLU()
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2)
+
+        self.conv3 = nn.Conv2d(ft_bank_baseline*2, ft_bank_baseline*4, kernel_size=3, padding='same')
+        self.act3 = nn.ReLU()
+        self.maxpool3 = nn.MaxPool2d(kernel_size=2)
+ 
+        self.flatten = nn.Flatten()
+        self.dropout = nn.Dropout(p=0.5)
+        self.fc1 = nn.Linear(inputsize * ft_bank_baseline*4, latent_dim*4) # regularize
+        self.act4 = nn.Tanh() 
+        
+        # z
+        self.feature_z_mean = nn.Linear(latent_dim*4, latent_dim*2)
+        self.actz1 = nn.Tanh()
+        self.z_mean_layer = nn.Linear(latent_dim*2, latent_dim)  
+        self.actz2 = nn.Tanh() 
+
+        self.feature_z_logvar = nn.Linear(latent_dim*4, latent_dim*2)
+        self.actz3 = nn.Tanh()
+        self.r_logvar_layer = nn.Linear(latent_dim*2, latent_dim)  
+        self.actz4 = nn.Tanh() 
+        
+        # r
+        self.feature_r_mean = nn.Linear(latent_dim*4, latent_dim*2)
+        self.actr1 = nn.Tanh()
+        self.r_mean_layer = nn.Linear(latent_dim*2, 1)   
+        self.actr2 = nn.Tanh() 
+
+        self.feature_r_logvar = nn.Linear(latent_dim*4, latent_dim*2)
+        self.actr3 = nn.Tanh()
+        self.r_logvar_layer = nn.Linear(latent_dim*2, 1)  
+        self.actr4 = nn.Tanh()
+
+        self.pz_mean_layer = nn.Linear(latent_dim*2, latent_dim)
+        self.pz_logvar_layer = nn.Linear(latent_dim*2, latent_dim)
+
+
+    def forward(self, x):
+
+        x = self.act1(self.conv1(x))
+        x = self.maxpool1(x)
+        x = self.act2(self.conv2(x))
+        x = self.maxpool2(x)
+        x = self.act3(self.conv3(x))
+        x = self.maxpool3(x)
+        x = self.flatten(x)
+        x = self.dropout(x)
+        x = self.act4(self.fc1(x))
+
+        z_mean_feature = self.actz1(self.feature_z_mean(x))
+        z_mean = self.actz2(self.z_mean_layer(z_mean_feature))
+        z_logvar_feature = self.actz3(self.feature_z_logvar(x))
+        z_logvar = self.actz4(self.z_logvar_layer(z_logvar_feature))
+
+        r_mean_feature = self.actr1(self.feature_r_mean(x))
+        r_mean = self.actr2(self.r_mean_layer(r_mean_feature))
+        r_logvar_feature = self.actr3(self.feature_r_logvar(x))
+        r_logvar = self.actr4(self.r_logvar_layer(r_logvar_feature))
+
+        z = sampling(z_mean, z_logvar)
+        r = sampling(r_mean, r_logvar) 
+
+        return z_mean, z_logvar, z, r_mean, r_logvar, r
+
+
+
+
         self.dropout = nn.Dropout(p=0.25)
         self.fc1 = nn.Linear(input_shape_x, 128)
         self.act1 = nn.Tanh()
